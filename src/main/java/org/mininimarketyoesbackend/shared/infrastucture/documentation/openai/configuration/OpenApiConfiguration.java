f8e5dfec1065ddb6248332ae8c92d1872263b5d9
YoesDevelop <ceticay7@gmail.com>
2024-06-14 10:32:06 -0500

    update properties

--- src/main/java/org/mininimarketyoesbackend/shared/infrastucture/documentation/openai/configuration/OpenApiConfiguration.java
+++ src/main/java/org/mininimarketyoesbackend/shared/infrastucture/documentation/openai/configuration/OpenApiConfiguration.java
@@ -27,7 +27,7 @@ public class OpenApiConfiguration  implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200/")
+                .allowedOrigins("https://minimarket-yoes.vercel.app/Front-End/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
